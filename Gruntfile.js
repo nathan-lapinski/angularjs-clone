ad60bd6711b6d7b80a6a5aa7a703893d33bb0d67
Nathan Lapinski <n4t311@gmail.com>
2018-04-25 23:42:34 +0900

    setup

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      all: ['src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          _: false,
+          $: false,
+	  jasmine: false,
+	  describe: false,
+	  it: false,
+	  expect: false,
+	  beforeEach: false,
+	  afterEach: false,
+	  sinon: false
+        },
+        browser: true,
+        devel: true
+      }
+    },
+    testem: {
+      unit: {
+        options: {
+          framework: 'jasmine2',
+          launch_in_dev: ['PhantomJS'],
+          before_tests: './node_modules/grunt/bin/grunt jshint',
+          serve_files: [
+            'node_modules/sinon/pkg/sinon.js',
+            'src/**/*.js',
+            'test/**/*.js'
+          ],
+          watch_files: [
+            'src/**/*.js',
+            'test/**/*'
+          ]
+        }
+      }
+    }
+  });
+
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-testem');
+};
