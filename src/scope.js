c21db80f436e153341cf1916e55aa0397f842d90
Nathan Lapinski <n4t311@gmail.com>
2018-04-26 17:59:43 +0900

    adding basic $watch and $digest functions to Scope

--- src/scope.js
+++ src/scope.js
@@ -2,5 +2,21 @@
 'use strict';
 
 function Scope() {
-	
+	this.$$watchers = [];
 }
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+	this.$$watchers.push({
+		watchFn: watchFn,
+		listenerFn: listenerFn
+	});
+};
+
+Scope.prototype.$digest = function() {
+	this.$$watchers.forEach(function(watcherObj){
+		// This is very basic
+		// We want to dirty check each watched value,
+		// and call the listenerFn only if that value has changed.
+		watcherObj.listenerFn();
+	});
+};
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -8,4 +8,23 @@ describe('Scope', function() {
   	scope.aProp = 1;
   	expect(scope.aProp).toBe(1);
   });
+
+  describe('digest', function() {
+  	var scope;
+
+  	beforeEach(function() {
+  		scope = new Scope();
+  	});
+
+  	it('calls the listener function of a watch on first $digest', function() {
+  		var watchFn = function() { return 'way'; };
+  		var listenerFn = jasmine.createSpy();
+  		scope.$watch(watchFn, listenerFn);
+
+  		scope.$digest();
+
+  		expect(listenerFn).toHaveBeenCalled();
+  	});
+  });
+
 });
